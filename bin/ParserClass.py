068ea38c2d14aa519d4cb7c198b37adba8d3ac0a
dpapadi <dpapadimass@gmail.com>
2018-04-12 10:39:32 -0700

    added bin dir with primary class

---
+++ bin/ParserClass.py
@@ -0,0 +1,11 @@
+import hashlib
+
+class Parser:
+    def __init__(self):
+        print "Class init"
+
+    def test(self):
+        print "Testing"
+        m = hashlib.md5()
+        print 
+        print m 
---
+++ bin/parse_it.py
@@ -0,0 +1,6 @@
+import sys
+from ParserClass import Parser
+
+if __name__ == '__main__':
+    t = Parser()
+    t.test()
