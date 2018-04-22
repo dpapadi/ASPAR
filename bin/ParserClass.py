4e5b11a21da61c1795c7a192d949070fecf12479
dpapadi <dpapadimass@gmail.com>
2018-04-22 09:01:00 -0700

    COMPLETED

--- bin/ParserClass.py
+++ bin/ParserClass.py
@@ -1,20 +1,97 @@
+import matplotlib.pyplot as plt
+
+class Node:
+    # object were data is stored in the LinkedList
+    # the Node contains the word count (word_cnt) and the document id of the doc (doc_id)
+    def __init__(self, doc_id = None, word_cnt = None):
+        self.doc_id = doc_id
+        self.word_cnt = word_cnt
+        self.next_node = None
+
+class LinkedList:
+    
+    def __init__(self, head = None):
+        # initialize by giving the head of the list or None if the list is empty
+        self.head = head
+        self.last_node = head
+
+    def insert(self, doc_id, word_cnt):
+        new_node = Node(doc_id, word_cnt)
+        if self.head == None:
+            self.head = new_node
+            self.last_node = new_node
+        else:
+            self.last_node.next_node = new_node
+            self.last_node = new_node
+
+    def search(self, doc_id):
+        tmp = self.head
+        found = False
+        while tmp:
+            print tmp.word_cnt
+            if tmp.doc_id == doc_id:
+                return tmp
+            else:
+                tmp = tmp.next_node
+        raise ValueError("doc_id not in LinkedList")
+
 class Parser:
 
+    #from . import LinkedList
     input_file = None   #input file
     __content = ""        # input file as string
     __doc_dict = {}       #dictionaty to store doc info
     __sec_id = [ 'doc', 'docno', 'docid', 'date', 'section', 'length', 'headline', 'byline', 'text', 'graphic', 'type' ]
+    #words = {}
+    __doc_array = []
+    __hash_table = {}
+    #created based on regular patterns of the english language; should be updated for more accuracy
+    #NOTE: I do not check for different forms in verbs, as patters like ed or ing can indicate adjectives
+    #       for this purpose, word tagging should be implimented
+    __word_ending_patterns ={   's'     :   '',
+                                'ss'    :   'ss',
+                                'as'    :   'as',
+                                'is'    :   'is',
+                                'us'    :   'us',
+                                'oes'   :   'o',
+                                'ies'   :   'y',
+                                'ves'   :   'f',
+                                'ous'   :   'ous',
+                                'sses'  :   'ss',
+                                'xes'   :   'x',
+                                'ches'  :   'ch',
+                                'shes'  :   'sh',
+                                'uses'  :   'us'
+
+                            }
+    # exceptions for the above rules
+    __exceptions = {    'its'       :   'its',
+                        'objectives':   'objective',
+                        'lies'      :   'lie',
+                        'yes'       :   'yes',
+                        'observes'  :   'observe',
+                        'moves'     :   'move',
+                        'men'       :   'man',
+                        'women'     :   'woman',
+                        'childern'  :   'child',
+                        'people'    :   'person',
+                        'am'        :   'be',
+                        'are'       :   'be',
+                        'is'        :   'be',
+                        'was'       :   'be',
+                        'were'      :   'be'
+                        }
+    test = 0
 
 
     def __init__(self, infile):
-        print "Class init\n"
         self.input_file = infile
         self.__content = self.input_file.read()
         self.input_file.close()
         self.__main_process()
-        print self.__doc_dict
-        (i, ii) = self.__doc_dict['LA010189-0013']['date'][1]
-        print self.__content[i:ii]
+        for i in range(len(self.__doc_array)): 
+            self.__process_doc(self.ret_doc(i), i+1)
+        print "\n\nINPUT collection has been parsed successfully!!!\n\n"
 
 
     def __main_process(self):
@@ -47,7 +124,9 @@ class Parser:
                 #print str(begin) + "\t" + str(end)
             if cur_docno != "":
                 self.__doc_dict[cur_docno]['position'] = i
+                self.__doc_array.append(cur_docno)
             i += 1
+            #print self.__doc_array
 
     def __get_sec(self, text, sec_id, con):
         ####################
@@ -106,22 +185,123 @@ class Parser:
                         start = ii
                         par_no += 1
                     
-    def ret_doc(self, docno, sec): #TODO
+    def ret_doc(self, docno): #TODO
         ret = ""
         temp = ""
         i = 1
         #for sid in self.__sec_id:
-        while True:
-            if i in self.__doc_dict[docno]['text']:
-                (begin, end) = self.__doc_dict[docno]['text'][i]
-                ret += self.__content[begin:end] + "\n"
-                i+=1
+        for sec_id in self.__sec_id:
+            if sec_id == 'doc' or sec_id == 'docno' or sec_id == 'docid':
+                continue
+            elif isinstance(self.__doc_dict[self.__doc_array[docno]][sec_id], dict):
+                while True:
+                    if i in self.__doc_dict[self.__doc_array[docno]][sec_id]:
+                        (begin, end) = self.__doc_dict[self.__doc_array[int(docno)]][sec_id][i]
+                        ret += self.__content[begin:end] + "\n"
+                        i+=1
+                    else:
+                        ret += "\n\n"
+                        i = 1
+                        break
             else:
-                break
+                (begin, end) = self.__doc_dict[self.__doc_array[docno]][sec_id]
+                ret += self.__content[begin:end] + "\n\n\n"
+        #self.__process_doc(ret)
         return ret
 
+    
+    def __process_doc(self, text, docno = None):
+        temp_word = ""
+        temp_dict = {}
+        count = 0
+        for i in range(len(text)):
+            ch = text[i]
+            #print ch
+            #raw_input()
+            if ch.isalnum() or ch =="'" or  ch == "-":
+                if ch.upper():
+                    ch = ch.lower()
+                temp_word += ch
+            else:
+                if temp_word != "":
+                    count += 1
+                    (proc_word, cnt_not) = self.__process_word(temp_word)
+                    if cnt_not:
+                        if 'not' not in temp_dict:
+                            temp_dict['not'] = 1
+                        else:
+                            temp_dict['not'] += 1
+                    if proc_word not in temp_dict:
+                        temp_dict[proc_word] = 1
+                    else:
+                        temp_dict[proc_word] += 1
+                temp_word = ""
+                #raw_input()
+        for word, cnt in temp_dict.items():
+            hsh = hash(word)
+            if hsh not in self.__hash_table:
+                self.__hash_table[hsh] = LinkedList()
+            self.__hash_table[hsh].insert(docno, cnt)
+        return
 
-    def test(self):
-        print "Testing"
-        print self.input_file
-        return self.input_file
+    def __process_word(self, inpt):
+        ############################
+        #
+        # before calculating the hash do a simple processing of the word to
+        # to find simple word ending patterns that indicate plural or other forms
+        # of the same word based on an preconfigured array. For more accuracy
+        # add more expressions to the array, a library for irregular forms and impliment word tagging to 
+        # discover the type of word ( noun, verb etc )
+        #
+        ###########################
+        outpt = inpt
+        count_not = False
+        if outpt.startswith("'") and not outpt[1:].isdigit():
+            outpt = outpt[1:]
+        while outpt.endswith("'"):
+            outpt = outpt[:-1]
+        if outpt[-2:] == "'s":
+            outpt = outpt[:-2]
+        elif outpt[-3:] == "n't":
+            outpt = outpt[:-3]
+            if outpt == "ca":
+                outpt = "can"
+            count_not = True
+        if outpt in self.__exceptions:
+            outpt = self.__exceptions[outpt]
+        elif outpt.endswith('s'):
+            k = -4
+            while k < 0:
+                if outpt[k:] in self.__word_ending_patterns:
+                    outpt = outpt[:k] + self.__word_ending_patterns[outpt[k:]]
+                    break
+                k += 1
+        
+        if outpt == 'the':
+            self.test += 1
+        return (outpt, count_not)
+
+    def plot_count_dist(self):
+        plot_data = {}
+        for hsh, linked_list in self.__hash_table.items():
+            total_count = 0
+            node = linked_list.head
+            while node != None:
+                total_count += node.word_cnt
+                node = node.next_node
+            if total_count not in plot_data:
+                plot_data[total_count] = 1
+            else:
+                plot_data[total_count] += 1
+        plt.bar(plot_data.keys(), plot_data.values(), color='g')
+        plt.show()
+
+
+    def return_data(self):
+        print '\n\n'
+        print 'Enter:'
+        for i in range(len(self.__doc_array)):
+            print '%s\tfor article %s' %(i+1, self.__doc_array[i])
+        print '0\tto return\n'
+        choice = raw_input()    
+        return choice
--- bin/parse_it.py
+++ bin/parse_it.py
@@ -3,12 +3,29 @@ from ParserClass import Parser
 
 if __name__ == '__main__':
 
-    in_file=open("/home/dpap/ASPAR/INPUT")
+    try:
+        in_file=open(sys.argv[1])
+    except:
+        print "\nProvide a valid INPUT file!!\n"
+        exit()
     my_parser = Parser(in_file)
     while True:
-        docno = raw_input("\n\n\n\nPlease provide the docno of the document you wish to see or 0 to quit: ")
-        if docno == '0':
-            exit()
-        else:
-            print
-            print my_parser.ret_doc(docno, 'text')
+        try:
+            choice = raw_input('\n\nEnter:\n1:\tto read a specific article\n2:\tto generate the word distribution plot\n0:\tto exit\n\n')
+            if choice == '0':
+                exit()
+            elif choice == '2':
+                my_parser.plot_count_dist()
+            elif choice == '1':
+                while True:
+                    docseq = my_parser.return_data()
+                    if docseq == '0':
+                        break
+                    else:
+                        print my_parser.ret_doc(int(docseq)-1)
+            else:
+                raise Exception
+        except Exception as e:
+                print "WRONG INPUT\n"
+                print 'DEBUG: ' 
+                print e 
